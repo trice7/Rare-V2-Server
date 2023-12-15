98c31b7a1c2befa872e136908c3c47a96f35a1c5
Kirthana Sukumaran <kirthanaa07@gmail.com>
2023-12-14 18:01:13 -0600

    merged

--- rareapi/models/post_tags.py
+++ rareapi/models/post_tags.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rareapi.models import Post, Tag
 
 class PostTag(models.Model):
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- rareapi/views/post_tags_view.py
+++ rareapi/views/post_tags_view.py
@@ -1,47 +1,47 @@
-# from django.http import HttpResponseServerError
-# from rest_framework.viewsets import ViewSet
-# from rest_framework.response import Response
-# from rest_framework import serializers, status
-# from rareapi.models import PostTag
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import PostTag
 
-# class PostTagView(ViewSet):
-#   def retrieve(self, request, pk):
-#     try:
-#       post_tag = PostTag.objects.get(pk=pk)
-#       serializer = PostTagSerializer(post_tag)
-#       return Response(serializer.data)
-#     except PostTag.DoesNotExist as ex:
-#       return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+class PostTagView(ViewSet):
+  def retrieve(self, request, pk):
+    try:
+      post_tag = PostTag.objects.get(pk=pk)
+      serializer = PostTagSerializer(post_tag)
+      return Response(serializer.data)
+    except PostTag.DoesNotExist as ex:
+      return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
     
-#   def list(self, request):
-#     post_tags = PostTag.objects.all()
-#     serializer = PostTagSerializer(post_tags, many=True)
-#     return Response(serializer.data)
+  def list(self, request):
+    post_tags = PostTag.objects.all()
+    serializer = PostTagSerializer(post_tags, many=True)
+    return Response(serializer.data)
   
-#   def create(self, request):
-#     post_tag = PostTag.objects.create(
-#       post=request.data["post_id"],
-#       tag=request.data["tag_id"],
-#     )
-#     serializer = PostTagSerializer(post_tag)
-#     return Response(serializer.data, status=status.HTTP_201_CREATED)
+  def create(self, request):
+    post_tag = PostTag.objects.create(
+      post=request.data["post_id"],
+      tag=request.data["tag_id"],
+    )
+    serializer = PostTagSerializer(post_tag)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
   
-#   def update(self, request, pk):
-#     post_tag = PostTag.objects.get(pk=pk)
-#     post_tag.post = request.data["post_id"]
-#     post_tag.tag = request.data["tag_id"]
-#     post_tag.save()
-#     serializer = PostTagSerializer(post_tag)
-#     return Response(serializer.data, status=status.HTTP_200_OK)
+  def update(self, request, pk):
+    post_tag = PostTag.objects.get(pk=pk)
+    post_tag.post = request.data["post_id"]
+    post_tag.tag = request.data["tag_id"]
+    post_tag.save()
+    serializer = PostTagSerializer(post_tag)
+    return Response(serializer.data, status=status.HTTP_200_OK)
   
-#   def destroy(self, request, pk):
-#     post_tag = PostTag.objects.get(pk=pk)
-#     post_tag.delete()
-#     return Response(None, status=status.HTTP_204_NO_CONTENT)
+  def destroy(self, request, pk):
+    post_tag = PostTag.objects.get(pk=pk)
+    post_tag.delete()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
   
-# class PostTagSerializer(serializers.ModelSerializer):
+class PostTagSerializer(serializers.ModelSerializer):
     
-#     class Meta:
-#       model = PostTag
-#       fields = ("id", "post", "tag")
+    class Meta:
+      model = PostTag
+      fields = ("id", "post", "tag")
 
