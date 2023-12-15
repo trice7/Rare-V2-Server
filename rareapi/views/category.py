9067ffc80433ebfc9555f9427a2cf6836f397c74
Mason Austin <mason47austin@gmail.com>
2023-12-14 18:36:22 -0600

    created list medthod in CategoryView

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -6,6 +6,20 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from rareapi.models import Category
 
+class CategoryView(ViewSet):
+    """Rare Category View"""
+    
+    def list(self, request):
+      """Handle GET requests for all categories
+      
+      Returns -> Response -- JSON serialized list of songs with status 200"""
+      
+      categories = Category.objects.all()
+      serializer = CategorySerializer(Category, many=True)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        
+
 class CategorySerializer(serializers.ModelSerializer):
     """JSON serializer for posts"""
     
