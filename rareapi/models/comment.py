3d21074fc77939fa7ed14e74bb59db0b84db4159
Eli Bradford <elibradford227@gmail.com>
2023-12-14 18:54:58 -0600

    fixed model fields

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -3,7 +3,7 @@ from .rare_user import rare_user
 from .post import Post
 
 class Comment(models.Model):
-    author_id = models.ForeignKey(rare_user, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(rare_user, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=250)
     created_on = models.DateTimeField()
