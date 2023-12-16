eb9ec8eff21008fa901db0fda26e3f90d0950674
Mason Austin <mason47austin@gmail.com>
2023-12-16 11:54:06 -0600

    altered user model to include isAdmin and user_name

--- rareapi/models/user.py
+++ rareapi/models/user.py
@@ -3,3 +3,5 @@ from django.db import models
 class User(models.Model):
     uid = models.CharField(max_length=50)
     bio = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+    isAdmin = models.BooleanField(False)
